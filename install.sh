60ede1ca1ceb955695e0038b0b39a644e02f23eb
thehnm <thehnm@posteo.net>
2024-10-19 01:37:26 +0200

    feat: remove lazygit files

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ install_lazygit() {
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
+    rm lazygit.tar.gz lazygit
     install lazygit "$HOME"/.local/bin
 }
 
